3eb051b250530fb727a9d6dcadfc116b515b8794
sardine323 <sardine323@gmail.com>
2019-05-17 18:24:46 +0800

    up

--- EXPT/EXPT/src/MergeSort.java
+++ EXPT/EXPT/src/MergeSort.java
@@ -10,17 +10,29 @@ public class MergeSort {
 		
 	}
 	
+	public static void sortBU(Comparable[] a)
+	{
+		aux = new Comparable[a.length];
+		for (int sz = 1; sz < a.length; sz = sz+sz)
+		{
+			for (int lo = 0; lo < a.length; lo = lo + sz)
+			{
+				merge(a, lo, lo+sz-1, Math.min(lo+sz+sz+1, a.length-1));
+			}
+		}
+	}
+	
 	private static void sort(Comparable[] a, int lo, int hi)
 	{
 		if (lo > hi) return;
 		int mid = (hi - lo)/2;
 		sort(a, lo, mid);
 		sort(a, mid+1, hi);
-		mergesort0(a, lo, mid, hi);
+		merge(a, lo, mid, hi);
 	}
 	// Top-down
 	// abstract in-place merge
-	public static Comparable[] mergesort0(Comparable[] a, int lo, int mid, int hi)
+	public static Comparable[] merge(Comparable[] a, int lo, int mid, int hi)
 	{
 		int i = lo;
 		int j = mid+1;
