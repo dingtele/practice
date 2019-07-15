ae5f441c36d677c79e5756c0be02c5f956c54223
sardine323 <sardine323@gmail.com>
2019-07-15 18:24:14 +0800

    Update Precolation.java

--- algorithm 4/Precolation.java
+++ algorithm 4/Precolation.java
@@ -1,74 +1,90 @@
 package pegasus;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 public class Percolation {
 
-	private Integer[][] location;
+	private int[][] location;
 	private int N;
-	private ArrayList<Integer> openList;
-	private Set<Integer> topVal;
+	private Set<Integer> openSet;
+	private WeightedQuickUnionUF uf;
+	
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
     	this.N = n;
-    	Set<Integer> topVal = new HashSet<Integer>();
-    	for(int j = 0; j < N; j++)
-    		topVal.add(location[0][j]);
-    	for(int i = 0; i < n; i++)
-    		for(int j = 0; i < n; j++)
-    			location[i][j] = (i-1)*n + (j-1);
+    	this.uf = new WeightedQuickUnionUF(N*N);
+    	location = new int[N][N];
+    	openSet = new HashSet<Integer>();
+    	for(int i = 0; i < N; i++) {
+    		for(int j = 0; j < N; j++) {
+    			location[i][j] = i*N + j;
+    		}
+    	}		
     }
 
     // opens the site (row, col) if it is not open already
-    public void open(int row, int col) {
-    	if ((row <= 0 || row > N) || col <= 0 || col > N) 	throw new IllegalArgumentException("invalid input!");
-    	if (!isOpen(row,col)) {	
-    		if(isOpen(row-1,col)) 			location[row][col] = location[row-1][col];
-    		else if(isOpen(row,col-1)) 		location[row][col] = location[row][col-1];
-    		else if(isOpen(row,col+1))  	location[row][col+1] = location[row][col];
-    		else if(isOpen(row+1,col))		location[row+1][col] = location[row][col];
-    		
-    		openList.add((row-1)*N + (col-1));
-    	}
+    public void open(int row, int col) {  	
+//TODO    	if ((row <= 0 || row > N) || (col <= 0 || col > N)) 	throw new IllegalArgumentException("invalid input!");
     	
-    	System.out.println("precolation threshold is" + openList.size() / N*N);
-	
-    	return;
+		int upperSite = (row-2)*N + (col-1);
+		int leftSite = (row-1)*N + (col-2);
+		int rightSite = (row-1)*N + col;
+		int downSite = row*N + (col-1);
+		int site = (row-1)*N + (col-1);
+		
+    	if (!isOpen(row,col)) {	  		
+        	if (row == 1) {
+        		for (int j = 0; j < N; j++) {
+        			location[row-1][j] = 0;// top row unified value
+        		}
+        	}else if (row == N) {
+        		for (int j = 0; j < N; j++) {
+        			location[row-1][j] = N*N-1;//TODO
+        		}
+        	}
+		
+    		if(isOpen(row-1,col) && row >= 2) 			uf.union(site, upperSite);// upper site is open
+    		else if(isOpen(row,col-1) && col >=2)		uf.union(leftSite, site);// left site is open
+    		else if(isOpen(row,col+1) && col <= N-1)  	uf.union(rightSite, site);// right site is open
+    		else if(isOpen(row+1,col) && row <= N-1)	uf.union(downSite, site);// down site is open
+    		
+    		openSet.add(Integer.valueOf((row-1)*N + (col-1)));
+    	}    
+    	System.out.println("precolation threshold is: " + openSet.size());
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-    	if ((row <= 0 || row > N) || col <= 0 || col > N) 	throw new IllegalArgumentException("invalid input!");
-    	return openList.contains((row-1)*N + (col-1));
+//    	if ((row <= 0 || row > N) || (col <= 0 || col > N)) 	throw new IllegalArgumentException("invalid input!");
+    	return openSet.contains(Integer.valueOf(row-1)*N + (col-1));
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-    	if ((row <= 0 || row > N) || col <= 0 || col > N) 	throw new IllegalArgumentException("invalid input!");
-    	return topVal.contains(location[row][col]);	
+//    	if ((row <= 0 || row > N) || (col <= 0 || col > N)) 	throw new IllegalArgumentException("invalid input!");
+    	if (row == 1) {	
+    			location[row-1][col-1] = 0;
+    			return true;
+    		}
+    	else if(!isOpen(row-1, col-1)) 	return false;
+    	else if (uf.connected((row-2)*N + (col-1), 0)) 	
+    			location[row-1][col-1] = 0;
+    			return true;
     }
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-    	return openList.size();
+    	return openSet.size();
     }
 
     // does the system percolate?
     public boolean percolates() {
-    	Set<Integer> bottVal = new HashSet<Integer>();
-    	Set<Integer> set = new HashSet<Integer>();
-    	for(int j = 0; j < N; j++)
-    		bottVal.add(location[N][j]);
-    	set.containsAll(bottVal);
-    	set.containsAll(topVal);
-    	return set.size() < (bottVal.size() +  topVal.size()); 	
+    	return location[N-1][N-1] == 0;
     }
 
     // test client (optional)
-    public static void main(String[] args) {
-    	StdIn.read
-    }
+//    public static void main(String[] args) {
+
+//    }
 }
