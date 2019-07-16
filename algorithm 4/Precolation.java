4303bd48b94dd5ec40a1685dbf22288913075f6b
sardine323 <sardine323@gmail.com>
2019-07-16 18:31:04 +0800

    Update Precolation.java

--- algorithm 4/Precolation.java
+++ algorithm 4/Precolation.java
@@ -5,72 +5,52 @@ import java.util.Set;
 
 public class Percolation {
 
-	private int[][] location;
-	private int N;
+	private int[] location;
+	private int n;
 	private Set<Integer> openSet;
 	private WeightedQuickUnionUF uf;
+	private int virtualTop;
+	private int virtualBottom;
 	
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
-    	this.N = n;
-    	this.uf = new WeightedQuickUnionUF(N*N);
-    	location = new int[N][N];
+    	if (n <= 0) 	throw new IllegalArgumentException("invalid input!");
+    	this.n = n;
+    	this.uf = new WeightedQuickUnionUF(n*n+2);
+    	location = new int[n*n+2];
     	openSet = new HashSet<Integer>();
-    	for(int i = 0; i < N; i++) {
-    		for(int j = 0; j < N; j++) {
-    			location[i][j] = i*N + j;
-    		}
+    	for(int i = 0; i < n*n+2; i++) {
+    			location[i] = i;
     	}		
+    	this.virtualTop = n*n+1;
+    	this.virtualBottom = n*n+2;
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {  	
-//TODO    	if ((row <= 0 || row > N) || (col <= 0 || col > N)) 	throw new IllegalArgumentException("invalid input!");
-    	
-		int upperSite = (row-2)*N + (col-1);
-		int leftSite = (row-1)*N + (col-2);
-		int rightSite = (row-1)*N + col;
-		int downSite = row*N + (col-1);
-		int site = (row-1)*N + (col-1);
-		
-    	if (!isOpen(row,col)) {	  		
-        	if (row == 1) {
-        		for (int j = 0; j < N; j++) {
-        			location[row-1][j] = 0;// top row unified value
-        		}
-        	}else if (row == N) {
-        		for (int j = 0; j < N; j++) {
-        			location[row-1][j] = N*N-1;//TODO
-        		}
-        	}
-		
-    		if(isOpen(row-1,col) && row >= 2) 			uf.union(site, upperSite);// upper site is open
-    		else if(isOpen(row,col-1) && col >=2)		uf.union(leftSite, site);// left site is open
-    		else if(isOpen(row,col+1) && col <= N-1)  	uf.union(rightSite, site);// right site is open
-    		else if(isOpen(row+1,col) && row <= N-1)	uf.union(downSite, site);// down site is open
-    		
-    		openSet.add(Integer.valueOf((row-1)*N + (col-1)));
-    	}    
-    	System.out.println("precolation threshold is: " + openSet.size());
+    	validateIndices(row, col);
+    	int i = xyTo1D(row, col);//convert to 1D index
+    	openSet.add(Integer.valueOf(i));
+    	if (row == 1) 	uf.union(virtualTop, i);
+    	if (isOpen(row-1,col) && row >= 2) 			uf.union(i, i-n); 	// upper site is open
+		else if (isOpen(row,col-1) && col >=2)		uf.union(i, i-1);// left site is open
+		else if (isOpen(row,col+1) && col <= n-1)  	uf.union(i, i+1);// right site is open
+		else if (isOpen(row+1,col) && row <= n-1)	uf.union(i, i+n); 	// down site is open	
+
+    	System.out.println("precolation threshold is: " + openSet.size());//
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-//    	if ((row <= 0 || row > N) || (col <= 0 || col > N)) 	throw new IllegalArgumentException("invalid input!");
-    	return openSet.contains(Integer.valueOf(row-1)*N + (col-1));
+    	validateIndices(row, col);
+    	return openSet.contains(Integer.valueOf(xyTo1D(row, col)));
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-//    	if ((row <= 0 || row > N) || (col <= 0 || col > N)) 	throw new IllegalArgumentException("invalid input!");
-    	if (row == 1) {	
-    			location[row-1][col-1] = 0;
-    			return true;
-    		}
-    	else if(!isOpen(row-1, col-1)) 	return false;
-    	else if (uf.connected((row-2)*N + (col-1), 0)) 	
-    			location[row-1][col-1] = 0;
-    			return true;
+    	validateIndices(row, col);   
+    	int i = xyTo1D(row, col);
+    	return (uf.connected(virtualTop, i));
     }
 
     // returns the number of open sites
@@ -80,11 +60,24 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates() {
-    	return location[N-1][N-1] == 0;
+    	return (uf.connected(virtualTop, virtualBottom));
     }
 
+    private int xyTo1D(int row, int col) {
+    	return (row-1)*n + (col-1);
+    }
+    
+    private void validateIndices(int row, int col) {
+//    	if (!(row <= n && row > 0)) 	throw new IllegalArgumentException("invalid input!");
+//    	if (!(col <= n && col > 0)) 	throw new IllegalArgumentException("invalid input!");
+    }
+    
     // test client (optional)
-//    public static void main(String[] args) {
-
-//    }
+    public static void main(String[] args) {
+    	int n = 2;
+    	Percolation per = new Percolation(n);
+    	per.open(1, 1);
+    	per.open(1, 2);
+    	System.out.println(per.uf.connected(0, 1));
+    }
 }
