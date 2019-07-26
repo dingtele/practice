7fcedf1acf87ff575857d6ac59c776aa13e7bff8
mcc4 <sardine323@gmail.com>
2019-07-26 18:24:28 +0800

    Update RandomizedQueue.java

--- algorithm 4/RandomizedQueue.java
+++ algorithm 4/RandomizedQueue.java
@@ -1,54 +1,107 @@
 package queues;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
-	private int N;
-	private int first;
-	private int last; 
-	private Item[] que;
+	private int n;
+	private Item[] que; // number of elements in the stack
 	
     // construct an empty randomized queue
     public RandomizedQueue() {
-    	que = (Item[]) new Object[0];
-    	N = que.length;
-    	first = 0;
-    	last = N-1;
+    	que = (Item[]) new Object[2];
+    	n = que.length;
     }
 
     // is the randomized queue empty?
     public boolean isEmpty() {
-    	return (N == 0);
+    	return (n == 0);
     }
 
     // return the number of items on the randomized queue
     public int size() {
-    	return N;
+    	return n;
     }
 
+    private Item[] resize(int capacity) {
+    	assert capacity >= 0;
+    	Item[] temp = (Item[]) new Object[capacity];
+    	for (int i = 0; i < n; i++) {
+    		temp[i] = que[i];
+    	}
+    	que = temp;
+    	return que;
+    }
+    
     // add the item
     public void enqueue(Item item) {
 		if (item == null) 	throw new IllegalArgumentException("wrong[addlast]");
-
-		
+		if (n == que.length) resize(2*que.length);	
+		que[n] = item;	
+		n++;
     }
 
     // remove and return a random item
     public Item dequeue() {
-    	Item item = StdRandom.random()
-    	
+    	if (isEmpty()) 		throw new NoSuchElementException("NoSuchElementException [deque]");
+    	int index = StdRandom.uniform(n);
+    	Item item = que[index];
+    	for (int i = index + 1; i < n; i++) {
+    		que[i-1] = que[i];
+    	}
+    	que[n-1] = null;
+    	n--;
+    	return item;
     }
 
     // return a random item (but do not remove it)
     public Item sample() {
-    	
+    	if (isEmpty()) 		throw new NoSuchElementException("NoSuchElementException [sample]");
+    	int index = StdRandom.uniform(n);
+    	Item item = que[index];
+    	return item;
     }
 
     // return an independent iterator over items in random order
-    public Iterator<Item> iterator()
+    public Iterator<Item> iterator(){
+    	return new ArrayIterator<Item>();
+    }
+    
+    private class ArrayIterator<Item> implements Iterator<Item>{
+
+    	private int current = 0;
+    	
+    	public boolean hasNext() {
+    		return (current < n-1);
+    	}
+    	
+    	public Item next() {
+    		if (!hasNext()) 	throw new NoSuchElementException("NoSuchElement[current]"); 
+    		Item item = (Item) que[current % que.length];
+    		current++;
+    		return item;
+    	}    	
+    }
 
     // unit testing (required)
-    public static void main(String[] args)
-
+    public static void main(String[] args) {
+    	RandomizedQueue<String> rq = new RandomizedQueue<String>();
+    	while (!StdIn.isEmpty()) {
+    		String item = StdIn.readString();
+    		if (!item.equals("-"))
+    			rq.enqueue(item);
+    		else if (!rq.isEmpty()) 	
+    			StdOut.println(rq.dequeue());
+    	}
+    	String sample = rq.sample();
+    	while (rq.iterator().hasNext()) {
+    		if (sample == rq.iterator().next());
+    	    StdOut.println(sample);	       	
+    	}	
+    }  	
 }
